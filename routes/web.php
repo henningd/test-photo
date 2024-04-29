8ff43b3672fdd2394f83663566a79d66d6a5929c
Daniel Henninger <info@dhe.de>
2024-04-29 17:44:41 +0200

    renamed file

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\WebcamController;
-use App\Http\Controllers\ImageController;
+use App\Http\Controllers\ImagesController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
