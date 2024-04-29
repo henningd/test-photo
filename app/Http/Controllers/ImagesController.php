53588cf0ae882134e22f9b220987792ef8251543
Daniel Henninger <info@dhe.de>
2024-04-29 17:34:07 +0200

    use storage

--- app/Http/Controllers/ImagesController.php
+++ app/Http/Controllers/ImagesController.php
@@ -2,12 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 
 class ImageController extends Controller
 {
     public function index()
     {
-        $images = File::allFiles(public_path('uploads'));
+        //$images = File::allFiles(public_path('uploads'));
+        $images = Storage::disk('public')->allFiles('uploads');
         dd($images);
         return view('images.index', compact('images'));
     }
